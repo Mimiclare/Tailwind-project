58ec4eeb05dd89975cc372b34f69b6c08c776f7b
Chinaza Miracle <mimiclarechinaza241@gmail.com>
2026-05-01 11:07:33 +0100

    Conditional program

---
+++ ass.js
@@ -0,0 +1,11 @@
+let number = Number(prompt("input the number"));
+
+if (number < 0) {
+    console.log('this number is negative')
+}
+else if(number > 0) {
+    console.log("this number is postive")
+}     
+else{
+    console.log("this number is zero")       
+}
