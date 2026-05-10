830e0aa086a370c47d4574a8974a51876207e980
Chinaza Miracle <mimiclarechinaza241@gmail.com>
2026-05-10 09:36:07 +0100

    updated filter and shopping cart

--- variable.js
+++ variable.js
@@ -22,19 +22,63 @@ function Cart() {
     this.items = [];
 }
 Cart.prototype.addItem = function(name, price) {
-    this.items {name: name, price: price}
+    this.items.push({name: name, price: price})
+}    
    
-} 
+Cart.prototype.removeItem = function(name) {
+    this.items = this.items.filter(function(item) {
+        return item.name !== name
+    })
+}
+
+Cart.prototype.getTotal = function(){
+    let total = 0
+    this.items.forEach(element => {
+        total = total + element.price
+    });
+    return total
+}
+
+Cart.prototype.displayCart = function(){
+    this.items.forEach(element => {
+        console.log(element.name + '-' +  element.price);
+
+    })
+    console.log ('Total: ' + this.getTotal())
+}
+const myCart = new Cart()
+
+myCart.addItem('Rice', 10000)
+myCart.addItem('Fried-Rice', 10000)
+myCart.addItem('Beans', 10000)
+myCart.addItem('Coconut-Rice', 10000)
+myCart.addItem('Abacha', 10000)
+myCart.displayCart()
+
+const findRice = myCart.items.filter(function(item){
+    return item.name === 'Rice'
+})
+
+console.log(findRice)
 
 
 
- { name: 'Rice',  Price: 10000 },
-  { name: 'Beans', Price: 9000 },
-  { name: 'Nsala', Price: 1000  },
-  { name: 'Egwusi', Price: 5000   },
-  { name: 'Amala',  Price: 6000 },
-  { name: 'Fried rice', Price: 100 },
-  { name: 'Miracle', Price: 125 },
+
+
+
+
+
+
+
+
+
+//  { name: 'Rice',  Price: 10000 },
+//   { name: 'Beans', Price: 9000 },
+//   { name: 'Nsala', Price: 1000  },
+//   { name: 'Egwusi', Price: 5000   },
+//   { name: 'Amala',  Price: 6000 },
+//   { name: 'Fried rice', Price: 100 },
+//   { name: 'Miracle', Price: 125 },
 
 
 
