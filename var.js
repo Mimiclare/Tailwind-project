6f5fe8beeec650246fbde9c6cc5c519d21c3d1d3
Chinaza Miracle <mimiclarechinaza241@gmail.com>
2026-05-11 17:26:51 +0100

    Arithmetic Operators

---
+++ var.js
@@ -0,0 +1,59 @@
+var number = 10;
+console.log(number)
+
+let name = "Miracle";
+console.log(name)
+
+const state = "false";
+console.log(state)
+
+// activity 3
+// activity 3
+let myNumber = 42;
+let myString = "Hello";
+let myBoolean = true;
+let myObject = { name: "Miracle" };
+let myArray = [1, 2, 3];
+
+console.log(typeof myNumber);   // "number"
+console.log(typeof myString);   // "string"
+console.log(typeof myBoolean);  // "boolean"
+console.log(typeof myObject);   // "object"
+console.log(typeof myArray);    // "object"
+
+// Activity 4
+let initial = "Kelvin";
+console.log(initial)
+initial = "J.V";
+console.log(initial)
+
+//  activity 5
+// const age = "Elizabeth"
+// console.log(age);
+
+// age = "Ella"
+// console.log(age);
+
+
+let add = 5+ 6
+console.log(add);
+
+
+let sub = 10 - 5
+console.log(sub);
+
+let mult = 10 * 5
+console.log(mult)
+
+let div = 10 / 5
+console.log(div);
+
+let mod = 100/6 mod 2
+console.log(mod)
+
+
+
+
+
+
+
